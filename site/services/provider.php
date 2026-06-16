2e597927d45bec20ccb7279250c7cd6435999ee6
smiginioklubas <smiginioklubas@gmail.com>
2026-06-16 09:41:51 +0300

    Backup before Joomla 5.4 rebuild

--- site/services/provider.php
+++ site/services/provider.php
@@ -2,12 +2,35 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\Router\RouterFactoryInterface;
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\Extension\Service\Provider\RouterFactory;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+use Smiginis\Component\Smiginis\Site\Extension\SmiginisComponent;
 
 return new class implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
+        $container->registerServiceProvider(new MVCFactory('Smiginis\\Component\\Smiginis'));
+        $container->registerServiceProvider(new ComponentDispatcherFactory('Smiginis\\Component\\Smiginis'));
+        $container->registerServiceProvider(new RouterFactory('Smiginis\\Component\\Smiginis'));
+
+        $container->set(
+            ComponentInterface::class,
+            function (Container $container) {
+                $component = new SmiginisComponent($container->get(ComponentDispatcherFactoryInterface::class));
+
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+                $component->setRouterFactory($container->get(RouterFactoryInterface::class));
+
+                return $component;
+            }
+        );
     }
 };
